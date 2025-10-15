6be1be9448888cac08742c676144e31c18aa915a
Ivan Prearo <ivan.prearo@hotmail.com>
2025-10-14 21:25:02 -0300

    Finalizacao projeto 3

--- projects/project3/Parametric_GVD/dispersion_analysis.py
+++ projects/project3/Parametric_GVD/dispersion_analysis.py
@@ -11,13 +11,14 @@ from scipy.differentiate import derivative
 from dispersion_functions import *
 
 
-FILE_PATH = './Accumulated_probe_table.csv'
-N_MODES = 4 # number of eigenvalues sought for in Comsol!!
+FILE_PATH = './24wv_SM_Accumulated_probe_table.csv'
+SAVE_PATH = './00_24wv'
+N_MODES = 1 # number of eigenvalues sought for in Comsol!!
 AUTO_CALCULATE_N_MODES = True # Automatically calculates the number of eigenvalues sought
 
 wavelength = np.linspace(0.5,1.4,1000)*u.um #
 wavelength0 = 1100*u.nm # pump wavelength
-λ0vec = np.linspace(1000,1200,200)*u.nm
+λ0vec = np.linspace(1000,1200,500)*u.nm
 
 '''
     TEMPORARY VALUES, WHILE WE FIGURE COMSOL CALCULATION
@@ -26,6 +27,21 @@ wavelength0 = 1100*u.nm # pump wavelength
 P=10*u.W # pump power
 L=5*u.km #waveguide length
 
+
+def new_interpolation(x, y):
+    interpolation = interpolate.interp1d(x,y)
+    def float_protec(z):
+        if np.any(z < np.min(x)):
+            print(f"Value outside interpolation range: {np.min(z)} " \
+                          f"is less than {np.min(x)}")
+            z = np.where(z < np.min(x), np.min(x), z)
+        if np.any(z > np.max(x)):
+            print(f"Value outside interpolation range: {np.max(z)} " \
+                          f"is more than {np.max(x)}")
+            z = np.where(z > np.max(x), np.max(x), z)
+        return interpolation(z)
+
+    return float_protec
 
 
 def index_analysis(df, save_path='./'):
@@ -36,7 +52,7 @@ def index_analysis(df, save_path='./'):
     fig,ax = plt.subplots(4,1,figsize=cm2inch(12,16),sharex=True)
     for i in range(N_MODES):
         #--------------
-        neff_list.append(np.real(df['Effective mode index (1), neff Probe'].iloc[i::N_MODES].values)) # take only real part
+        neff_list.append(np.real(df['Effective_mode_index (1)'].iloc[i::N_MODES].values)) # take only real part
         ng, β1, β2, D = GVDwg(lambda_vec ,neff_list[i])
         β2_list.append(β2) # we will need this for plotting later
         #--------------
@@ -53,16 +69,17 @@ def index_analysis(df, save_path='./'):
         #---
         ax0=ax[2]
         ax0.plot(lambda_vec[:-2].to(u.um),β2,'-',label="mode {:}".format(i))
-        ax0.set_ylabel('GVD (β2) - $ps^2/km$')
+        ax0.set_ylabel('GVD [$ps^2/km$]')
         ax0.grid(True)
         #---
         ax0=ax[3]
         ax0.plot(lambda_vec[:-2].to(u.um),D,'-',label="mode {:}".format(i))
-        ax0.set_ylabel('D - $ps/nm.km$')
-        ax0.set_xlabel(r'Wavelength($\mu$m)')    
+        ax0.set_ylabel('D [$ps/nm.km$]')
+        ax0.set_xlabel(r'Wavelength [$\mu$m]')    
         ax0.grid(True)
 
-    ax[2].legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)    
+    if N_MODES > 1:
+        ax[2].legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)    
 
     save_file = os.path.join(save_path, 'dispersion.png')
     plt.savefig(save_file, bbox_inches='tight', dpi=300)
@@ -75,12 +92,12 @@ def index_analysis(df, save_path='./'):
 def beta2_analysis(df, β2_list, lambda_vec, save_path='./'):
     #Interpolation function for β2
     for β2 in  β2_list:
-        beta2interp = interpolate.interp1d(lambda_vec[:-2], β2) #note that this has units of psˆ2/km!!
+        beta2interp = new_interpolation(lambda_vec[:-2], β2) #note that this has units of psˆ2/km!!
         #validating interpolation
         plt.plot(lambda_vec[:-2].to(u.um),beta2interp(lambda_vec[:-2]) )
         plt.plot(lambda_vec[:-2].to(u.um),β2,'*')
-    plt.xlabel("Wavelegnth (μm)")
-    plt.ylabel("β2 (ps$^2$/nm/km)")
+    plt.xlabel("Wavelength [μm]")
+    plt.ylabel("$β_2$ [$ps^2nm^{-1}km^{-1}$]")
     plt.grid()
 
     save_file = os.path.join(save_path, 'beta_2.png')
@@ -95,7 +112,7 @@ def deltabeta(wavelength, wavelength0, β2_list, lambda_vec, imode):
     #wavelength0: pump wavelength - must be within the lambda_vec range!!
     β2=β2_list[imode-1] # here we choose the last mode, because it has the highest refractive index!
     #!!!astropy cannot attribute units to interp1d objects, but the values will be in psˆ2/km!!!!!
-    beta2interp = interpolate.interp1d(lambda_vec[:-2], β2) 
+    beta2interp = new_interpolation(lambda_vec[:-2], β2) 
     #once calculated, we can restore units!!
     beta2λp=beta2interp(wavelength0.to(u.m))*u.ps**2/u.km #note that this has units of psˆ2/km!!
     freq=2*pi*cte.c/wavelength # frequency vector
@@ -113,12 +130,12 @@ def deltabeta_analysis(wavelength, wavelength0, β2_list, lambda_vec, imode=N_MO
     fig,ax = plt.subplots(1,2,figsize=cm2inch(14,6),sharey=True)
     ax0=ax[0]
     ax0.plot(wavelength.to(u.um),deltabeta_values.to(1/u.mm))
-    ax0.set_xlabel("Wavelegnth (μm)")
-    ax0.set_ylabel("Δβ (mm)$^{-1}$")
+    ax0.set_xlabel("Wavelength [μm]")
+    ax0.set_ylabel("Δβ [$mm^{-1}$]")
     ax0.grid()
     ax0=ax[1]
     ax0.plot(freq.to(u.THz),deltabeta_values.to(1/u.mm))
-    ax0.set_xlabel("Frequency(THz)")
+    ax0.set_xlabel("Frequency [THz]")
     ax0.grid()
 
     save_file = os.path.join(save_path, 'deltabeta.png')
@@ -135,12 +152,12 @@ def deltabeta_pump_analysis(wavelength, β2_list, lambda_vec, imode=N_MODES, sav
         deltabeta_values = deltabeta(wavelength,wavelength0,β2_list,lambda_vec,imode)
         ax0=ax[0]
         ax0.plot(wavelength.to(u.um),deltabeta_values.to(1/u.mm))
-        ax0.set_xlabel("Wavelegnth (μm)")
-        ax0.set_ylabel("Δβ (mm)$^{-1}$")
+        ax0.set_xlabel("Wavelength [μm]")
+        ax0.set_ylabel("Δβ [$mm^{-1}$]")
         ax0.grid()
         ax0=ax[1]
         ax0.plot(freq.to(u.THz),deltabeta_values.to(1/u.mm),label="λ$_0$={:}".format(wavelength0))
-        ax0.set_xlabel("Frequency(THz)")
+        ax0.set_xlabel("Frequency [THz]")
         ax0.grid()
         plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
 
@@ -156,6 +173,7 @@ def g0(wavelength,wavelength0, β2_list, lambda_vec, γ, P, imode=N_MODES):
 def gain(wavelength,wavelength0, β2_list, lambda_vec, γ, P, L, imode=N_MODES):
     gvec = g0(wavelength,wavelength0, β2_list, lambda_vec, γ, P, imode)
     return 1+8*(γ*P/gvec)**2*np.sinh( (gvec*L/2).value )**2
+
 
 
 def gain_analysis(wavelength, wavelength0, β2_list, lambda_vec, γ, P, L, imode=N_MODES, save_path='./'):
@@ -177,20 +195,21 @@ def gain_analysis(wavelength, wavelength0, β2_list, lambda_vec, γ, P, L, imode
     #------------------
     ax0=ax[0]
     ax0.plot(wavelength.to(u.um),Δβ)
-    ax0.axhline((-2*γ*P).to(1/u.mm).value,c='red')
-    ax0.set_ylabel("Δβ (mm)$^{-1}$")
+    for g in γ:
+        ax0.axhline((-2*g*P).to(1/u.mm).value,c='red')
+    ax0.set_ylabel("Δβ [$mm^{-1}$]")
     ax0.grid()
     #------------------
     ax0=ax[1]
     ax0.plot(wavelength.to(u.um),np.real(gvec))
-    ax0.set_ylabel("Gain coefficient (m)$^{-1}$")
+    ax0.set_ylabel("Gain coeff. [$m^{-1}$]")
     ax0.grid()
     #------------------
     ax0=ax[2]
     ax0.plot(wavelength.to(u.um),np.real(GaindB))
     ax0.grid()
-    ax0.set_ylabel("Gain (dB)")
-    ax0.set_xlabel("Wavelegnth (μm)")
+    ax0.set_ylabel("Gain [dB]")
+    ax0.set_xlabel("Wavelength [μm]")
 
     save_file = os.path.join(save_path, 'gain_wavelength.png')
     plt.savefig(save_file, bbox_inches='tight', dpi=300)
@@ -204,20 +223,21 @@ def gain_analysis(wavelength, wavelength0, β2_list, lambda_vec, γ, P, L, imode
     #------------------
     ax0=ax[0]
     ax0.plot(dλ,Δβ)
-    ax0.axhline((-2*γ*P).to(1/u.mm).value,c='red')
-    ax0.set_ylabel("Δβ (mm)$^{-1}$")
+    for g in γ:
+        ax0.axhline((-2*g*P).to(1/u.mm).value,c='red')
+    ax0.set_ylabel("Δβ [$mm^{-1}$]")
     ax0.grid()
     #------------------
     ax0=ax[1]
     ax0.plot(dλ,np.real(gvec))
-    ax0.set_ylabel("Gain coefficient (m)$^{-1}$")
+    ax0.set_ylabel("Gain coeff. [$m^{-1}$]")
     ax0.grid()
     #------------------
     ax0=ax[2]
     ax0.plot(dλ,np.real(GaindB))
     ax0.grid()
-    ax0.set_ylabel("Gain (dB)")
-    ax0.set_xlabel("Δλ (λ-λ$_p$) (μm)")
+    ax0.set_ylabel("Gain [dB]")
+    ax0.set_xlabel("Δλ (λ-λ$_p$) [μm]")
     
     save_file = os.path.join(save_path, 'gain_w-wp.png')
     plt.savefig(save_file, bbox_inches='tight', dpi=300)
@@ -243,36 +263,37 @@ def gain_pump_analysis(wavelength, wavelength0, β2_list, lambda_vec, γ, P, L, 
         #------------------
         ax0=ax[0]
         ax0.plot(wavelength.to(u.um),Δβ)
-        ax0.axhline((-2*γ*P).to(1/u.mm).value,c='red')
-        ax0.set_ylabel("Δβ (mm)$^{-1}$")
+        for g in γ:
+            ax0.axhline((-2*g*P).to(1/u.mm).value,c='black')
+        ax0.set_ylabel("Δβ [$mm^{-1}$]")
         ax0.grid()
         #------------------
         ax0=ax[1]
         ax0.plot(wavelength.to(u.um),np.real(gvec),label="λ$_0$={:}".format(wavelength0))
-        ax0.set_ylabel("Gain coefficient (m)$^{-1}$")
+        ax0.set_ylabel("Gain coeff. [$m^{-1}$]")
         ax0.grid()
         #------------------
         ax0=ax[2]
         ax0.plot(wavelength.to(u.um),np.real(GaindB))
         ax0.grid()
-        ax0.set_ylabel("Gain (dB)")
-        ax0.set_xlabel("Wavelegnth (μm)")
+        ax0.set_ylabel("Gain [dB]")
+        ax0.set_xlabel("Wavelength [μm]")
     ax[1].legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.) 
         
-    save_file = os.path.join(save_path, 'paramteric_gain_lp_sweep.png')
+    save_file = os.path.join(save_path, 'parametric_gain_lp_sweep.png')
     plt.savefig(save_file, bbox_inches='tight', dpi=300)
     plt.clf()
     plt.close()
 
 
 def zero_GVD_wl(wavelength, β2_list, lambda_vec, imode=N_MODES):
-    beta2interp = interpolate.interp1d(lambda_vec[:-2], β2_list[imode-1]) 
-    index=np.argmin(beta2interp(wavelength.to(u.m))>0)
+    beta2interp = new_interpolation(lambda_vec[:-2], β2_list[imode-1]) 
+    index=np.argmin(beta2interp(wavelength.to(u.m))**2)
     return wavelength[index].to(u.nm)
 
 
 def gain_density_analysis(wavelength, β2_list, λ0vec, imode=N_MODES, save_path='./'):
-    beta2interp = interpolate.interp1d(lambda_vec[:-2], β2_list[imode-1]) 
+    beta2interp = new_interpolation(lambda_vec[:-2], β2_list[imode-1]) 
     GainMatrix=np.zeros([len(wavelength),len(λ0vec)])
     for i,wavelength0 in enumerate(λ0vec):
         Gain= gain(wavelength,wavelength0, β2_list, lambda_vec, γ, P, L, imode)
@@ -284,14 +305,14 @@ def gain_density_analysis(wavelength, β2_list, λ0vec, imode=N_MODES, save_path
     fig,ax = plt.subplots(1,2,sharey=True, gridspec_kw={'width_ratios': [0.5, 1]})
     ax0=ax[0]
     ax0.plot(beta2interp(λ0vec.to(u.m))*u.ps**2/u.km,λ0vec.value)
-    ax0.set_ylabel("Pump Wavelegnth (μm)")
-    ax0.set_xlabel("β2 (ps$^2$/nm/km)")
+    ax0.set_ylabel("Pump Wavelength [μm]")
+    ax0.set_xlabel("$β_2$ [$ps^2$/nm/km]")
     ax0.grid()
     ax0=ax[1]
     c = ax0.pcolormesh(wavelength.value,λ0vec.value , np.transpose(GainMatrix), cmap='plasma', vmin=GainMatrix.min(), vmax=GainMatrix.max(), rasterized=True)
     plt.colorbar(c, label='Norm. Intensity')
     ax0.grid()
-    ax0.set_xlabel("Wavelegnth (μm)")
+    ax0.set_xlabel("Wavelength [μm]")
         
     save_file = os.path.join(save_path, 'parametric_gain_density_wavelength.png')
     plt.savefig(save_file, bbox_inches='tight', dpi=300)
@@ -300,7 +321,7 @@ def gain_density_analysis(wavelength, β2_list, λ0vec, imode=N_MODES, save_path
 
 
 def gain_density_shift_analysis(wavelength, β2_list, λ0vec, imode=N_MODES, save_path='./'):
-    beta2interp = interpolate.interp1d(lambda_vec[:-2], β2_list[imode-1]) 
+    beta2interp = new_interpolation(lambda_vec[:-2], β2_list[imode-1]) 
     GainMatrix=np.zeros([len(wavelength),len(λ0vec)])
     λMatrix=np.zeros([len(wavelength),len(λ0vec)])
     λ0Matrix=np.zeros([len(wavelength),len(λ0vec)])
@@ -316,17 +337,56 @@ def gain_density_shift_analysis(wavelength, β2_list, λ0vec, imode=N_MODES, sav
     fig,ax = plt.subplots(1,2,sharey=True, gridspec_kw={'width_ratios': [0.5, 1]})
     ax0=ax[0]
     ax0.plot(beta2interp(λ0vec.to(u.m))*u.ps**2/u.km,λ0vec.value)
-    ax0.set_ylabel("Pump Wavelegnth (μm)")
-    ax0.set_xlabel("β2 (ps$^2$/nm/km)")
+    ax0.set_ylabel("Pump Wavelength [μm]")
+    ax0.set_xlabel("$β_2$ [$ps^2$/nm/km]")
     ax0.grid()
     ax0=ax[1]
     c = plt.pcolormesh(λMatrix,λ0Matrix, (GainMatrix), cmap='plasma', vmin=GainMatrix.min(), vmax=GainMatrix.max(), rasterized=True)
-    plt.colorbar(c, label='Gain (dB)')
+    plt.colorbar(c, label='Gain [dB]')
     ax0.grid()
     ax0.set_xlim([-0.4,0.4])
-    ax0.set_xlabel("λ-λ$_p$ (μm)")
+    ax0.set_xlabel("λ-λ$_p$ [μm]")
         
     save_file = os.path.join(save_path, 'parametric_gain_density_wavelength_shift.png')
+    plt.savefig(save_file, bbox_inches='tight', dpi=300)
+    plt.clf()
+    plt.close()
+
+
+    
+def gain_density_3plots_analysis(wavelength, β2_list, λ0vec, imode=N_MODES, save_path='./'):
+    beta2interp = new_interpolation(lambda_vec[:-2], β2_list[imode-1]) 
+    GainMatrix=np.zeros([len(wavelength),len(λ0vec)])
+    λMatrix=np.zeros([len(wavelength),len(λ0vec)])
+    λ0Matrix=np.zeros([len(wavelength),len(λ0vec)])
+    for i,wavelength0 in enumerate(λ0vec):
+        Gain= gain(wavelength,wavelength0, β2_list, lambda_vec, γ, P, L, imode)
+        GaindB=10*np.log10(Gain)
+        #----
+        GainMatrix[:,i]=np.real(GaindB)
+        λMatrix[:,i]=wavelength-wavelength0
+        λ0Matrix[:,i]=np.ones(len(wavelength))*wavelength0
+
+
+    fig,ax = plt.subplots(1,3,sharey=True, gridspec_kw={'width_ratios': [0.5, 1, 1]})
+    ax0=ax[0]
+    ax0.plot(beta2interp(λ0vec.to(u.m))*u.ps**2/u.km,λ0vec.value)
+    ax0.set_ylabel("Pump Wavelength [μm]")
+    ax0.set_xlabel("$β_2$ [$ps^2$/nm/km]")
+    ax0.grid()
+    ax0=ax[1]
+    c = ax0.pcolormesh(wavelength.value,λ0vec.value , np.transpose(GainMatrix), cmap='plasma', vmin=GainMatrix.min(), vmax=GainMatrix.max(), rasterized=True)
+    # plt.colorbar(c, label='Norm. Intensity')
+    ax0.grid()
+    ax0.set_xlabel("Wavelength [μm]")
+    ax0=ax[2]
+    c = ax0.pcolormesh(λMatrix,λ0Matrix, (GainMatrix), cmap='plasma', vmin=GainMatrix.min(), vmax=GainMatrix.max(), rasterized=True)
+    plt.colorbar(c, label='Gain [dB]')
+    ax0.grid()
+    ax0.set_xlim([-0.4,0.4])
+    ax0.set_xlabel("λ-λ$_p$ [μm]")
+        
+    save_file = os.path.join(save_path, 'parametric_gain_density_wavelength_3plot.png')
     plt.savefig(save_file, bbox_inches='tight', dpi=300)
     plt.clf()
     plt.close()
@@ -368,8 +428,11 @@ if __name__ == '__main__':
 
     for d_core in D_core_values:
 
+        print(f"\n\nStarting analysis of D_core={d_core:.2e}")
+
         df = raw_df.loc[raw_df['D_core (m)'] == d_core]
-        save_dir = f'./{d_core*1e6:.2f}um diameter'
+        save_dir = os.path.join(SAVE_PATH, f'{d_core*1e6:.2f}um diameter')
+        os.makedirs(SAVE_PATH, exist_ok=True)
         os.makedirs(save_dir, exist_ok=True)
 
         wv0_values = np.unique(df['% wv0 (m)'])
@@ -379,6 +442,14 @@ if __name__ == '__main__':
         neff_list, β2_list, lambda_vec = index_analysis(df, save_dir)
         zero_GVD = zero_GVD_wl(wavelength, β2_list, lambda_vec, imode=N_MODES)
 
+        γ_calc = np.array(df["Nonlinear coefficient"]) /(u.m * u.W)
+        if N_MODES > 1:
+            γ = np.average(γ_calc)*np.ones_like(wavelength.value)
+            print("Using average value for gamma: ", np.average(γ_calc))
+        else:
+            γ_interp = new_interpolation(wv0_values*u.m, γ_calc)
+            γ = γ_interp(wavelength)/(u.m * u.W)
+
         beta2_analysis(df, β2_list, lambda_vec, save_dir)
 
         deltabeta_analysis(wavelength, wavelength0, β2_list, lambda_vec, save_path=save_dir)
@@ -387,5 +458,8 @@ if __name__ == '__main__':
         gain_analysis(wavelength, wavelength0, β2_list, lambda_vec, γ, P, L, imode=N_MODES, save_path=save_dir)
         gain_pump_analysis(wavelength, wavelength0, β2_list, lambda_vec, γ, P, L, imode=N_MODES, save_path=save_dir)
 
+        print(f"Value of ZDW: {zero_GVD:.3e}")
+
         gain_density_analysis(wavelength, β2_list, λ0vec, imode=N_MODES, save_path=save_dir)
         gain_density_shift_analysis(wavelength, β2_list, λ0vec, imode=N_MODES, save_path=save_dir)
+        gain_density_3plots_analysis(wavelength, β2_list, λ0vec, imode=N_MODES, save_path=save_dir)
